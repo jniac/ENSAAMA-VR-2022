de6dc706a4746a4522a873985d31675b16d38966
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-05-12 15:42:42 +0200

    debug device input

---
+++ Assets/Contents/Scripts/XRInputDebug.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR;
+
+[ExecuteAlways]
+public class XRInputDebug : MonoBehaviour
+{
+    void PrintDevices() 
+    {
+        // https://docs.unity3d.com/Manual/xr_input.html
+        var devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices)
+        GetComponent<TMPro.TextMeshProUGUI>().text = "devices:\n" + 
+            string.Join("\n", devices.Select(d => $"device: {d.name} role: {d.characteristics}"));
+    }
+
+    void Start()
+    {
+        PrintDevices();
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ENSAAMA VR 2022
+
+## Links
+- https://docs.unity3d.com/Manual/xr_input.html
+- https://developer.oculus.com/documentation/unity/unity-tutorial/
