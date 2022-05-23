d5fff852720be2822e186681b2c0f50c74033535
jniac <joseph.merdrignac@gmail.com>
2022-05-23 09:27:39 +0200

    SimpleRotation is cooler

--- Assets/Contents/Scripts/SimpleRotation.cs
+++ Assets/Contents/Scripts/SimpleRotation.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class SimpleRotation : MonoBehaviour
 {
+    public enum RotationMode
+    {
+        Local,
+        Global,
+    }
+
+    public RotationMode mode = RotationMode.Local;
+    public Vector3 axis = Vector3.up;
     public float angularVelocity = 100;
 
     void Update()
     {
-        transform.localRotation *= Quaternion.Euler(0, angularVelocity * Time.deltaTime, 0);    
+        switch (mode)
+        {
+            case RotationMode.Local:
+                transform.localRotation *= Quaternion.Euler(axis * angularVelocity * Time.deltaTime);
+                break;
+            case RotationMode.Global:
+                transform.rotation *= Quaternion.Euler(axis * angularVelocity * Time.deltaTime);
+                break;
+        }
     }
 }
